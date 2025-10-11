ab376b79e55f9166cfa1272a7a395733abdfcc1c
kjannette <kjannette@kjannettes-MacBook-Air.attlocal.net>
2025-10-10 21:04:08 -0400

    yolo

--- app.js
+++ app.js
@@ -1,224 +1,34 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const fs = require("fs");
-const cors = require("cors");
-const multer = require("multer");
-
 // Import API services
 const { postReelToFacebook } = require("./postServices/facebookApiService.js");
 const {
   postReelToInstagram,
 } = require("./postServices/instagramApiService.js");
 //const { db } = require("./firebase/firebase.js");
-//const crypto = require("crypto");
 
 const { collection, query, where, getDocs } = require("firebase/firestore");
+const port = 3200
 
-const generatePassword = (
-  length = 20,
-  characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$"
-) =>
-  Array.from(crypto.randomFillSync(new Uint32Array(length)))
-    .map((x) => characters[x % characters.length])
-    .join("");
-
-//*** STRIPE ***/
-
-/*
-const Stripe = require("stripe");
-const { stripeAPIKey, stripeWebhooksKey } = require("./firebase/secrets.js");
-const stripe = Stripe(stripeAPIKey);
-// Secret for Stripe webhooks
-const endpointSecret = stripeWebhooksKey;
-const httpProxy = require("http-proxy");
-const targetUrl = "http://127.0.0.1:8081";
-const proxy = httpProxy.createProxy({
-  changeOrigin: true,
-  target: targetUrl,
-});
-*/
-
-//*** MULTER ***/
-const storage = multer.diskStorage({
-  destination: "./Documents/Uploads",
-  filename: function (req, file, callback) {
-    callback(null, file.originalname);
-  },
-});
-
-const altStorage = multer.diskStorage({
-  destination: "./Documents/Complaints",
-  filename: function (req, file, callback) {
-    callback(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-const uploadComp = multer({ storage: altStorage });
-
-/*
- *  POST to Generate Docx
- */
-
-const rootDir =
-  process.env.NODE_ENV === "development"
-    ? "/Users/kjannette/workspace8/testStoraage"
-    : "/var/www";
-
-//*** EXPRESS ***/
-
-const port = 3200;
-
-var corsOptions = {
-  AccessControlAllowOrigin: "*",
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Serve video files statically for API testing
-app.use("/video_files", express.static("video_files"));
-
-/*
- *  Client POST create stripe subscription, make payment
- */
-
-app.post("/create-subscription", async (req, res) => {
-  const { planType, additionalAccounts, isAnnual, customerData, token } =
-    req.body;
-  try {
-    const sub = await stripeController.createNewSubscription(
-      planType,
-      additionalAccounts,
-      isAnnual,
-      customerData,
-      token
-    );
-
-    const subscriptionCreated = sub.subscription.created;
-    const subscriptionPeriodStart = sub.subscription.current_period_start;
-    const subscriptionPeriodEnd = sub.subscription.current_period_end;
-    const subscriptionId = sub.subscription.id;
-    const customerId = sub.customer.customerId;
-
-    res.send({
-      subscriptionCreated,
-      subscriptionPeriodStart,
-      subscriptionPeriodEnd,
-      subscriptionId,
-      customerId,
-    });
-  } catch (error) {
-    console.log("Error in create-subscription", error);
-    res.status(400).send({ error: { message: error.message } });
-  }
-});
-
-/*
- *  Client POST create stripe subscription, make payment
- */
-console.log("process.env.NODE_ENV", process.env.NODE_ENV);
-app.post("/new-payment-intent", async (req, res) => {
-  const { planType, additionalAccounts, isAnnual, customerData, token } =
-    req.body;
-
-  const userAgent = req.headers["user-agent"];
-  try {
-    const payIntent = await stripeController.createNewPaymentIntent(
-      customerData,
-      token,
-      userAgent
-    );
-    res.send({
-      payIntent,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).send({ error: { message: error.message } });
-  }
-});
-
-/*
- *  Client POST for cancelling a subscription
- */
-
-app.post("/cancel-subscription", async (req, res) => {
-  const { appUserId } = req.body;
-  try {
-    //const usersRef = collection(db, "users");
-    //const q = query(usersRef, where("appUserId", "==", appUserId));
-    //const querySnapshot = await getDocs(q);
-
-    if (querySnapshot.empty) {
-      console.log("No user found with the email:", appUserId);
-      return;
-    }
-
-    const userDoc = querySnapshot.docs[0];
-    //get the user's subscription ID and customer ID
-    const subscriptionId = userDoc.data().subscriptionId;
-    const deletedSubscription = await stripe.subscriptions.update(
-      subscriptionId,
-      {
-        cancel_at_period_end: true,
-      }
-    );
-
-    res.status(200).send();
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({ error: { message: error.message } });
-  }
-});
-
-/*
- *  Client POST - Stripe webhook(s)
- */
-
-app.post(
-  "/stripe-webhook",
-  express.raw({ type: "application/json" }),
-  async (request, response) => {
-    const { type } = request.body.type;
-    switch (request.body.type) {
-      case "customer.subscription.deleted":
-        const subscription = request.body.data.object;
-
-        //get stripe customer
-        const stripeCustomer = await stripe.customers.retrieve(
-          subscription.customer
-        );
-
-        await handleSubscriptionDeletion(stripeCustomer, subscription, stripe);
-        break;
-        x;
-      case "invoice.payment_failed":
-        const paymentIntent = request.body.data.object;
-        await handlePaymentFailure(paymentIntent);
-        break;
-      default:
-        console.log(`Unhandled event type`);
-    }
-    response.status(200).send();
-  }
-);
-
+// Middleware
+app.use(cors()); // Enable CORS for all origins
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 /*
  *  POST store new reel data
  */
 
-app.post("/v1/store-reel-data/:reelId", function (req, res) {
+app.post("/v1/accept-reel-data/:reelId", function (req, res) {
+  console.log("accept-reel-data was hit by frontend")
   const { reelId } = req.params;
   const data = req.body;
   try {
     //storeReelData(docId, data);
-    console.log("data received by backed:  ", data);
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~`data received by backed:  ", data);
   } catch (err) {
-    console.log("Error at /v1/store-edited-completions:", err);
+    console.log("Error at /v1/store-reel-data/:reelId:", err);
   }
   res.end();
 });
@@ -232,8 +42,8 @@ app.post("/test/facebook-reel", async function (req, res) {
 
   try {
     // Construct the public URL for the video file
-    const baseUrl = `https://www.midnightsoldiers.com:${port}`;
-    const videoUrl = `${baseUrl}/video_files/${videoFileName}`;
+    //const baseUrl = `https://www.midnightsoldiers.com:${port}`;
+    //const videoUrl = `${baseUrl}/video_files/${videoFileName}`;
 
     // Get file size
     const filePath = `./video_files/${videoFileName}`;
@@ -438,6 +248,5 @@ app.put("/api/post-to-social/:reelId", async function (req, res) {
 });
 
 console.log("app running on port", port);
-console.log("rootDir", rootDir);
 
 app.listen(port);
